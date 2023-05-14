b30197b07d5a267d4b042670f296058a9830a367
Kamila Szewczyk <27734421+kspalaiologos@users.noreply.github.com>
2023-05-14 08:34:59 +0200

    Create ExampleManagedObject.java

---
+++ java/src/main/java/palaiologos/example/ExampleManagedObject.java
@@ -0,0 +1,42 @@
+package palaiologos.example;
+
+class ExampleManagedObject {
+    static {
+        try {
+            load(resourceName());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    // Free the pointer when object is out of scope.
+    static class CleanerRunnable implements Runnable {
+        private final long pointer;
+
+        CleanerRunnable(long pointer) {
+            this.pointer = pointer;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Freeing ExampleManagedObject");
+            ExampleManagedObject.free(pointer);
+        }
+    }
+
+    private final long ptr;
+    private final Cleaner.Cleanable cleanable;
+
+    private static native long alloc(String s, int n);
+    private static native void free(long ptr);
+    private static native String frobnicate(long ptr);
+
+    public ExampleManagedObject(String s, int n) {
+        System.out.println("Allocating ExampleManagedObject");
+        this.ptr = alloc(s, n);
+        cleanable = CleanerSingleton.CLEANER.register(this, new CleanerRunnable(ptr));
+    }
+
+    public String frobnicate() { return frobnicate(ptr); }
+}
+

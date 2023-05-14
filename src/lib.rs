631f49ea69c5f084fe77247a21d8a7bc76330ca2
Kamila Szewczyk <27734421+kspalaiologos@users.noreply.github.com>
2023-05-14 08:41:31 +0200

    Fix uses

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,10 @@ use jni::JNIEnv;
 // These objects are what you should use as arguments to your native
 // function. They carry extra lifetime information to prevent them escaping
 // this context and getting used after being GC'd.
-use jni::objects::{JClass, JString};
-use jni::sys::{jint, jlong};
+use jni::objects::{JClass, JString, JObject};
+
+// Raw objects.
+use jni::sys::{jint, jlong, jstring};
 
 #[no_mangle]
 pub extern "system" fn Java_palaiologos_example_Example_printHi(_env: JNIEnv, _class: JClass) {
